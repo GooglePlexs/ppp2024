5f7a380c6d3b540e86289b73b6eeb64fc80d3fbf
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-03-06 06:32:36 +0000

    1주차 수업 코드

---
+++ lec01/format_ex.py
@@ -0,0 +1,6 @@
+name = "john smith"
+department = "스마트팜학과"
+day = "수요일"
+print("{}님 안녕하세요. 오늘은 {}입니다".format(name,"수요일"))
+print("{}님 좋은 하루 되세요.".format(name)) 
+print("{} ({})님 안녕하세요!".format(name, department))
