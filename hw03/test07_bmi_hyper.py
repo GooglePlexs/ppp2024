cc806935bbcad31e6f340080c40f6f732ef22319
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-03-19 11:00:46 +0000

    3주차 첫번째 과제

--- hw03/test07_bmi_hyper.py
+++ hw03/test07_bmi_hyper.py
@@ -1,4 +1,5 @@
 import math
+
 def calculate_bmi_point(weight,height_m):
     return weight / math.pow (height_m , 2)
 
@@ -10,13 +11,13 @@ bmi = calculate_bmi_point(weight,height_m)
 
 if bmi < 18.5:
     print(print("키가 {}cm, 몸무게가 {}kg이면 BMI는 {:.1f}이며 저체중입니다.".format(height_cm,weight,bmi)))
-elif bmi <23:
+elif bmi < 23:
     print(print("키가 {}cm, 몸무게가 {}kg이면 BMI는 {:.1f}이며 표준체형입니다.".format(height_cm,weight,bmi)))
-elif bmi <25:
+elif bmi < 25:
     print(print("키가 {}cm, 몸무게가 {}kg이면 BMI는 {:.1f}이며 비만 전단계입니다.".format(height_cm,weight,bmi)))
-elif bmi <30:
+elif bmi < 30:
     print(print("키가 {}cm, 몸무게가 {}kg이면 BMI는 {:.1f}이며 1단계 비만입니다.".format(height_cm,weight,bmi)))
-elif bmi <35:
+elif bmi < 35:
     print(print("키가 {}cm, 몸무게가 {}kg이면 BMI는 {:.1f}이며 2단계 비만입니다.".format(height_cm,weight,bmi)))
 else:
     print(print("키가 {}cm, 몸무게가 {}kg이면 BMI는 {:.1f}이며 3단계 비만입니다.".format(height_cm,weight,bmi)))
