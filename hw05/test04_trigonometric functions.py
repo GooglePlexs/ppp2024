ddb88be1ebf3052e8f7a215f598a6a17c1f63895
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-03-23 11:56:49 +0000

    3주차 두번째 과제

--- hw05/test04_trigonometric functions.py
+++ hw05/test04_trigonometric functions.py
@@ -1,17 +1,18 @@
 import math
 
 start_degree = 0
-end_degree = 90
+end_degree = 360
 
 
 print("각도 radian    sin      cos      tan")
 
 
 for x in range(start_degree, end_degree + 1):
+  radian_result = math.radians(x)
   sin_result = math.sin(math.radians(x))
   cos_result = math.cos(math.radians(x))
   tan_result = math.tan(math.radians(x))
-  radian_result = math.radians(x)
+  
 
   
   print(f"{x}   {radian_result:.4f}   {sin_result:.4f}   {cos_result:.4f}   {tan_result:.4f}")
