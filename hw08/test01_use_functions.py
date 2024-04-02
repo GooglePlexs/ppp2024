cd0f565c11354616e767b3a0321646adc68b4663
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-04-02 11:02:06 +0000

    5주차 첫번째 과제

--- hw08/test01_use_functions.py
+++ hw08/test01_use_functions.py
@@ -16,6 +16,15 @@ def median(nums):
   
   return sorted_nums[mid_index]
 
+def min_number(nums):
+    min_num = min(nums)
+    return min_num
+
+def max_number(nums):
+    max_num = max(nums)
+    return max_num
+
+
 def main():
   
   input_text = "5 10 3 4 7"
@@ -25,15 +34,15 @@ def main():
   print(f"주어진 리스트는 {nums}")
 
   average_value = average(nums)
-  print(f"평균값은 {average_value:.1f}")
+  print("평균값은 {:.1f}".format(average_value))
 
   median_value = median(nums)
-  print(f"중앙값은 {median_value}")
+  print("중앙값은 {}".format(median_value))
 
-  min_value = min(nums)
+  min_value = min_number(nums)
   print(f"최솟값은 {min_value}")
 
-  max_value = max(nums)
+  max_value = max_number(nums)
   print(f"최댓값은 {max_value}")
 
 if __name__ == "__main__":
