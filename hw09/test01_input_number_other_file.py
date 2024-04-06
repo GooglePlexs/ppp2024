aaf40c8ca8e95082f91a2b692e0e0bf66a45b916
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-04-06 03:24:23 +0000

    5주차 두번째 과제

--- hw09/test01_input_number_other_file.py
+++ hw09/test01_input_number_other_file.py
@@ -8,6 +8,7 @@ def read_file(filename):
             data.extend(nums)
         return data
 
+
 def text2list(text):
     return [int(x) for x in text.split()]
 
@@ -19,6 +20,13 @@ def average(nums):
 
   return sum(nums) / len(nums)
 
+def max_number(nums):
+    max_num = max(nums)
+    return max_num
+
+def min_number(nums):
+    min_num = min(nums)
+    return min_num
 
 def median(nums):
 
@@ -28,14 +36,6 @@ def median(nums):
   
   return sorted_nums[mid_index]
 
-def min_number(nums):
-    min_num = min(nums)
-    return min_num
-
-def max_number(nums):
-    max_num = max(nums)
-    return max_num
-
 
 def main():
     nums = read_file("hw09/numbers1.txt")
@@ -44,16 +44,17 @@ def main():
     print(f"총 숫자 개수: {num_value}")
 
     average_value = average(nums)
-    print(f"평균값: {average_value:.1f}")
+    print(f"평균: {average_value:.1f}")
+
+    max_value = max_number(nums)
+    print(f"최댓값: {max_value}")
+
+    min_value = min_number(nums)
+    print(f"최솟값: {min_value}")
 
     median_value = median(nums)
     print(f"중앙값: {median_value}")
 
-    min_value = min_number(nums)
-    print(f"최솟값: {min_value}")
-    
-    max_value = max_number(nums)
-    print(f"최댓값: {max_value}")
 
 if __name__ == "__main__":
     main()
