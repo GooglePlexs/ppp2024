376939e32a923e721ca531f9edfec8c4d985aa6a
GooglePlexs <93317692+GooglePlexs@users.noreply.github.com>
2024-04-06 03:13:30 +0000

    5주차 두번째 과제

---
+++ hw09/test01_input_number_other_file.py
@@ -0,0 +1,59 @@
+def read_file(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+        data = []
+        for line in lines:
+            text = line.strip()
+            nums = text2list(text)
+            data.extend(nums)
+        return data
+
+def text2list(text):
+    return [int(x) for x in text.split()]
+
+def num_number(nums):
+    
+    return len(nums)
+
+def average(nums):
+
+  return sum(nums) / len(nums)
+
+
+def median(nums):
+
+  sorted_nums = sorted(nums)
+  length = len(sorted_nums)
+  mid_index = length // 2
+  
+  return sorted_nums[mid_index]
+
+def min_number(nums):
+    min_num = min(nums)
+    return min_num
+
+def max_number(nums):
+    max_num = max(nums)
+    return max_num
+
+
+def main():
+    nums = read_file("hw09/numbers1.txt")
+    
+    num_value = num_number(nums)
+    print(f"총 숫자 개수: {num_value}")
+
+    average_value = average(nums)
+    print(f"평균값: {average_value:.1f}")
+
+    median_value = median(nums)
+    print(f"중앙값: {median_value}")
+
+    min_value = min_number(nums)
+    print(f"최솟값: {min_value}")
+    
+    max_value = max_number(nums)
+    print(f"최댓값: {max_value}")
+
+if __name__ == "__main__":
+    main()
